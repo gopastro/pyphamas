615d8cd30125603d6332e3e7bdce38aec63dd7e4
Gopal Narayanan <gopal@astro.umass.edu>
2015-10-23 11:00:06 -0400

    Add skeletal framework for GBT FITS files

---
+++ pyphamas/gbtfits/gbtfits.py
@@ -0,0 +1,20 @@
+"""
+A set of utilities to extract important
+information from GBT FITS files
+"""
+
+import pyfits
+import os
+
+class GBTFITS(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.open_fits()
+
+    def open_fits(self):
+        if not os.path.exists(self.filename):
+            raise Exception("FITS file %s not found" % self.filename)
+        self.hdulist = pyfits.open(self.filename)
+        print "FITS file %s opened" % self.filename
+        print self.hdulist.info()
+
