4a278eac7f251182e82e8918c0b335c85484b563
Gopal Narayanan <gopal@astro.umass.edu>
2016-05-23 15:13:39 -0400

    Interpolation for time

--- pyphamas/gbtfits/quad_det_fits.py
+++ pyphamas/gbtfits/quad_det_fits.py
@@ -23,15 +23,15 @@ class QuadrantDetectorFITS(GBTFITS):
         """
         if (self.hdulist[1].header['EXTNAME'].strip()  != 'QDATA'):
             raise Exception("Method only applies for Quadrant Data FITS file")        
-        self.total_offset = self.hdulist['QDATA'].data['total_offset']
-        self.mc_total_offset = self.hdulist['QDATA'].data['mc_total_offset']
+        self._total_offset = self.hdulist['QDATA'].data['total_offset']
+        self._mc_total_offset = self.hdulist['QDATA'].data['mc_total_offset']
 
     def mc_total_offset(self, time):
         """
         Given a time array, reinterpolates intrinsic time array
         to given time array and return mc_total_offset
         """
-        return numpy.interp(time, self.quadTimes, self.mc_total_offset)
+        return numpy.interp(time, self.quadTimes, self._mc_total_offset)
 
         
 
